fef73bf7817a92679099b49f1ea25c5a16d9da65
Juanse Franco <jsfc2199@gmail.com>
2024-06-26 16:24:13 -0500

    template y ts del formulario reactivo

--- src/app/country/pages/selector-page/selector-page.component.ts
+++ src/app/country/pages/selector-page/selector-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-selector-page',
@@ -6,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class SelectorPageComponent {
 
+    constructor(private fb: FormBuilder){}
+
+    form: FormGroup = this.fb.group({
+      region: ['', Validators.required],
+      country: ['', Validators.required],
+      borders: ['', Validators.required],
+
+    })
 }
